414ba2aac4fa9ba325f886a55e279d11f0da44d5
0age <0age@protonmail.com>
2020-03-13 11:48:22 -0600

    initial scaffolding

---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+var Web3 = require("web3");
+
+module.exports = {
+  networks: {
+    development: {
+      provider: new Web3("ws://localhost:8545"),
+      network_id: "*"
+    },
+    coverage: {
+      provider: new Web3("ws://localhost:8555"),
+      network_id: "*"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.16",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  },
+  mocha: {
+    enableTimeouts: false
+  },
+  plugins: ["solidity-coverage"]
+};
