bbba48052b1366b5a5db8c8cb198038b25764358
0age <0age@protonmail.com>
2020-03-13 21:30:18 -0600

    make naming more explicit for SimpleFlopper

--- contracts/SimpleFlopper.sol
+++ contracts/SimpleFlopper.sol
@@ -4,36 +4,37 @@ import "./IFlopper.sol";
 
 contract SimpleFlopper {
 
-  IFlopper private _flopper;
+  // A "flopper" is a contract for auctioning off MKR in exchange for Dai.
+  IFlopper private _auction;
 
-  constructor(address flopper) public {
-    _flopper = IFlopper(flopper);
+  constructor(address auction) public {
+    _auction = IFlopper(auction);
   }
 
   // Getters //
 
   /// @notice Get the status of the flopper contract
-  /// @return bool status true if flopper contract is enabled
+  /// @return bool status true if auction contract is enabled
   function isEnabled() public view returns (bool status) {
-    return (_flopper.live() == 1) ? true : false;
+    return (_auction.live() == 1) ? true : false;
   }
 
   /// @notice Get the id of the latest auction
   /// @return auctionID uint256 id
-  function getNumAuctions() public view returns (uint256 auctionID) {
-    return _flopper.kicks();
+  function getTotalNumberOfAuctions() public view returns (uint256 auctionID) {
+    return _auction.kicks();
   }
 
   /// @notice Get the address of the MKR token
   /// @return MKR address of maker token
   function getMKRAddress() public view returns (address MKR) {
-    return _flopper.gem();
+    return _auction.gem();
   }
 
   /// @notice Get the address of the DAI token
   /// @return DAI address of maker token
-  function getDAIAddress() public view returns (address DAI) {
-    return _flopper.vat();
+  function getDaiAddress() public view returns (address DAI) {
+    return _auction.vat();
   }
 
   /// @notice Get the flopper contract config
@@ -41,13 +42,13 @@ contract SimpleFlopper {
   /// @return repriceIncrement uint256 reprice increment as percentage (initial = 1.50E18)
   /// @return bidDuration uint256 duration of a bid in seconds (initial = 3 hours)
   /// @return auctionDuration uint256 initial duration of an auction in seconds (initial = 2 days)
-  function getConfig() public view returns (
+  function getAuctionInformation() public view returns (
     uint256 bidIncrement,
     uint256 repriceIncrement,
     uint256 bidDuration,
     uint256 auctionDuration
   ) {
-    return (_flopper.beg(), _flopper.pad(), _flopper.ttl(), _flopper.tau());
+    return (_auction.beg(), _auction.pad(), _auction.ttl(), _auction.tau());
   }
 
   /// @notice Get the winning bid for an auction
@@ -63,7 +64,7 @@ contract SimpleFlopper {
     uint48 bidDeadline,
     uint48 auctionDeadline
   ) {
-    IFlopper.Bid memory bid = _flopper.bids(auctionID);
+    IFlopper.Bid memory bid = _auction.bids(auctionID);
     return (bid.bid, bid.lot, bid.guy, bid.tic, bid.end);
   }
 
@@ -72,22 +73,22 @@ contract SimpleFlopper {
   /// @notice Extend and reprice expired auction with no bid
   /// @dev state machine: after auction expiry, before first bid
   /// @param auctionID uint256 id of the auction
-  function reprice(uint256 auctionID) internal {
-    _flopper.tick(auctionID);
+  function _reprice(uint256 auctionID) internal {
+    _auction.tick(auctionID);
   }
 
   /// @notice Add bid to a live auction, if first bid this transfers DAI to vat
   /// @dev state machine: before auction expired
   /// @param auctionID uint256 id of the auction
-  function bid(uint256 auctionID, uint256 amountMKR, uint256 amountDAI) internal {
-    _flopper.dent(auctionID, amountMKR, amountDAI);
+  function _bid(uint256 auctionID, uint256 amountMKR, uint256 amountDAI) internal {
+    _auction.dent(auctionID, amountMKR, amountDAI);
   }
 
   /// @notice Finalize an auction with a winning bid and release maker
   /// @dev state machine: after auction expired
   /// @param auctionID uint256 id of the auction
-  function finalize(uint256 auctionID) internal {
-    _flopper.deal(auctionID);
+  function _finalize(uint256 auctionID) internal {
+    _auction.deal(auctionID);
   }
 
 }
