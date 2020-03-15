1b122b4b75543e993048bd6c5936a64357d7d460
0age <0age@protonmail.com>
2020-03-15 10:54:32 -0600

    simplify activation

--- interfaces/DaiBackstopSyndicateInterface.sol
+++ interfaces/DaiBackstopSyndicateInterface.sol
@@ -4,8 +4,7 @@ pragma solidity 0.5.16;
 interface DaiBackstopSyndicateInterface {
   enum Status {
     ACCEPTING_DEPOSITS,
-    ACTIVATED,
-    DEACTIVATED
+    ACTIVATED
   }
 
   // Anyone can deposit Dai up until the auctions have started at 1:1
@@ -14,17 +13,11 @@ interface DaiBackstopSyndicateInterface {
   // Anyone can withdraw at any point as long as Dai is not locked in auctions
   function defect(uint256 backstopTokenAmount) external returns (uint256 daiRedeemed, uint256 mkrRedeemed);
 
-  // Anyone can activate the contract once auctions have started, stopping deposits and enabling bids
-  function activate() external;
-
   // Anyone can enter an auction, supplying 50,000 Dai in exchange for 500 MKR
   function enterAuction(uint256 auctionId) external;
 
   // (may not be necessary since this is just dent, no tend?)
   function finalizeAuction(uint256 auctionId) external;
-
-  // Anyone can deactivate the ability to make new bids once auctions finish
-  function deactivate() external;
 
   function getStatus() external view returns (Status status);
 
